cec73edd532f2bbed602b416e9aa9c2f912fb43c
jinwoo <hjw3012@naver.com>
2020-03-05 19:50:29 +0900

    lifecycle

--- src/App.js
+++ src/App.js
@@ -4,9 +4,34 @@ import Three from "./components/Three";
 import StateExample from "./components/StateExample";
 import Button from "./components/Button";
 import "./App.css";
+import Counter from "./components/Counter";
+import NewCounter from "./components/NewCounter";
 
-function App() {
-  return <div></div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 10 };
+    this.resetCount = this.resetCount.bind(this);
+  }
+
+  resetCount() {
+    this.setState(({ count }) => ({ count: count + 10 }));
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          <Counter count={this.state.count} />
+        </div>
+        <div>
+          <NewCounter count={this.state.count} />
+        </div>
+        <button onClick={this.resetCount}>
+          {this.state.count + 10}으로 초기화
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
